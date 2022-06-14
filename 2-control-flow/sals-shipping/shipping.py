2d7c67da650558743f6e48d34ea9733138ef82ce
julhassk <58595014+julhassk@users.noreply.github.com>
2022-06-14 17:46:47 +0530

    Update shipping.py

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,31 @@
 # Sal's Shipping
-# Sonny Li
+# Julhas Sk
 
-weight = 80
+weight = 41.5
 
-# Ground Shipping 🚚
+# Ground Shipping 
+if weight <= 2 :
+  cost_ground = weight * 1.50 + 20.00
+elif 2 < weight <= 6:
+  cost_ground = weight * 3.00 + 20.00
+elif 6 < weight <= 10:
+  cost_ground = weight * 4.00 + 20.00
+elif weight > 10 :
+  cost_ground = weight * 4.75 + 20.00
 
-if weight <= 2:
-  cost_ground = weight * 1.5 + 20
-elif weight <= 6:
-  cost_ground = weight * 3.00 + 20
-elif weight <= 10:
-  cost_ground = weight * 4.00 + 20
-else:
-  cost_ground = weight * 4.75 + 20
+print('Ground Shipping $ ',cost_ground)
 
-print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
-
+# Ground Shipping Premimum
 cost_ground_premium = 125.00
+print("Ground Shipping Premium $", cost_ground_premium)
 
-print("Ground Shipping Premimium $", cost_ground_premium)
-
-# Drone Shipping 🛸
-
-if weight <= 2:
-  cost_drone = weight * 4.5
-elif weight <= 6:
-  cost_drone = weight * 9.00
-elif weight <= 10:
-  cost_drone = weight * 12.00
-else:
-  cost_drone = weight * 14.25
-
-print("Drone Shipping: $", cost_drone)
+# Drone Shipping
+if weight <= 2 :
+  cost_drone = weight * 4.50 + 0.00
+elif 2 < weight <= 6:
+  cost_drone = weight * 9.00 + 0.00
+elif 6 < weight <= 10:
+  cost_drone = weight * 12.00 + 0.00
+elif weight > 10 :
+  cost_drone = weight * 14.25 + 0.00
+print('Drone Shipping: $ ',cost_drone)
